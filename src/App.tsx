848c406256bc56eb09e1b84cdbbb0299882677cc
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-07-03 20:06:05 -0300

    feat(App): implement `QueryClientProvider`

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components';
 import theme from 'assets/styles/theme';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Router />
+      <QueryClientProvider client={queryClient}>
+        <Router />        
+      </QueryClientProvider>
       </BrowserRouter>
     </ThemeProvider>
   );
