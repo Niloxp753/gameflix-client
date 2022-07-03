5d1aa1f3f94a4dbdd473e5dd9a4b65f02aa2e665
Danilo Diniz <97798047+Niloxp753@users.noreply.github.com>
2022-07-03 19:05:24 -0300

    chose: create `router.tsx`

---
+++ src/router.tsx
@@ -0,0 +1,12 @@
+import { Route, Routes } from "react-router-dom";
+import { RoutePath } from "types/routes";
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path={RoutePath.HOME} element={<Home />} />
+        </Routes>
+    )
+}
+
+export default Router;
